7ee6ebae9c10a1d2888744b301c1b070c5d2e146
Divij <divij.mehra.16@gmail.com>
2017-06-17 15:52:41 +1000

    added navbar component

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,27 +1,19 @@
-
 import React, { Component } from 'react';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 import './App.css';
+import FaArrowLeft from 'react-icons/lib/fa/arrow-left';
+import style from './Navigation.css'
 
 class Navigation extends Component {
   render() {
     return (
+      <div className="navbarComponent">
+        <div className="brand"><FaArrowLeft /></div>
 
-        <div>
-        <Navbar color="faded" light>
-            <NavbarBrand href="/"><i class="fa fa-arrow-left" aria-hidden="true"></i></NavbarBrand>
-            <Nav navbar>
-              <NavItem>
-                <NavLink href="/components/">Components</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
-              </NavItem>
-            </Nav>
-        </Navbar>
       </div>
     );
   }
+
 }
 
 export default Navigation;
