904c7771a4e613e47727b99069929b64103c5c1f
Hyun Kim <hkim@live.com.au>
2017-06-17 11:35:21 +1000

    init

---
+++ src/LoginPage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class LoginPage extends Component {
+  render() {
+    return (
+      <div>
+        <img src="" alt=""/>
+        <button></button>
+        <h2></h2>
+        <input type="text"/>
+        <input type="text"/>
+        <button></button>
+        <p></p>
+      </div>
+    );
+  }
+}
+
+export default LoginPage;
